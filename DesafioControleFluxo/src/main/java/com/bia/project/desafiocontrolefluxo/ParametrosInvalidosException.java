10b37f981424b40cb0ed9ee28e74d5206f961d3d
Bia <biaemerich22@gmail.com>
2025-01-22 16:17:54 -0300

    First Commit

---
+++ DesafioControleFluxo/src/main/java/com/bia/project/desafiocontrolefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package com.bia.project.desafiocontrolefluxo;
+
+public class ParametrosInvalidosException extends RuntimeException {
+  public ParametrosInvalidosException(String message) {
+    super(message);
+  }
+}
---
+++ DesafioControleFluxo/src/test/java/com/bia/project/desafiocontrolefluxo/DesafioControleFluxoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bia.project.desafiocontrolefluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioControleFluxoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
